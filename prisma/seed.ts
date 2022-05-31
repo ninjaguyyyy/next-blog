7c8beacdf2192ad178a5c6f03e24dbe6c2c1e597
Dung Le <phieudu241@gmail.com>
2022-05-31 09:45:31 +0700

    Initialize project

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  // Seed roles
+  const user = await prisma.role.upsert({
+    where: { role: "user" },
+    update: {},
+    create: {
+      role: "user",
+    },
+  });
+
+  const admin = await prisma.role.upsert({
+    where: { role: "admin" },
+    update: {},
+    create: {
+      role: "admin",
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
